9ffd8d744275a93fce012aed4bdda1515ded8b08
artur.haack <artur.haack@r211-139.schule.local>
2019-04-09 11:53:18 +0200

    Buttonfunktionalität erweitert

--- MCG Quizchampion/src/GUI_Start.java
+++ MCG Quizchampion/src/GUI_Start.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.ButtonGroup;
 
 public class GUI_Start extends JFrame {
 
@@ -26,6 +27,8 @@ public class GUI_Start extends JFrame {
 	private JButton btnZweiSpieler;
 	private JButton btnDreiSpieler;
 	private JTextField txtSpielername3;
+	private final ButtonGroup btnGroupSpielerZahl = new ButtonGroup();
+
 
 	/**
 	 * Launch the application.
@@ -77,18 +80,30 @@ public class GUI_Start extends JFrame {
 		contentPane.add(btnStart);
 		
 		txtSpielername1 = new JTextField();
+		txtSpielername1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HauptKlasse.setSpielername1(txtSpielername1.getText());
+				
+			}
+		});
 		txtSpielername1.setText("Spielername 1");
 		txtSpielername1.setBounds(54, 218, 100, 20);
 		contentPane.add(txtSpielername1);
 		txtSpielername1.setColumns(10);
 		
 		txtSpielername2 = new JTextField();
+		txtSpielername2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HauptKlasse.setSpielername2(txtSpielername2.getText());
+			}
+		});
 		txtSpielername2.setText("Spielername 2");
 		txtSpielername2.setBounds(54, 307, 100, 20);
 		contentPane.add(txtSpielername2);
 		txtSpielername2.setColumns(10);
 		
 		btnEinSpieler = new JButton("1 Spieler");
+		btnGroupSpielerZahl.add(btnEinSpieler);
 		btnEinSpieler.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				HauptKlasse.setSpielerZahl(1);
@@ -99,18 +114,33 @@ public class GUI_Start extends JFrame {
 		contentPane.add(btnEinSpieler);
 		
 		btnZweiSpieler = new JButton("2 Spieler");
+		btnGroupSpielerZahl.add(btnZweiSpieler);
+		btnZweiSpieler.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HauptKlasse.setSpielerZahl(2);
+				System.out.println("Zwei Spieler gewaehlt!");
+			}
+		});
 		btnZweiSpieler.setBounds(54, 273, 89, 23);
 		contentPane.add(btnZweiSpieler);
 		
 		btnDreiSpieler = new JButton("3 Spieler");
+		btnGroupSpielerZahl.add(btnDreiSpieler);
 		btnDreiSpieler.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				HauptKlasse.setSpielerZahl(3);
+				System.out.println("Drei Spieler gewaehlt!");
 			}
 		});
 		btnDreiSpieler.setBounds(54, 356, 89, 23);
 		contentPane.add(btnDreiSpieler);
 		
 		txtSpielername3 = new JTextField();
+		txtSpielername3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				HauptKlasse.setSpielername3(txtSpielername3.getText());
+			}
+		});
 		txtSpielername3.setText("Spielername 3");
 		txtSpielername3.setBounds(54, 390, 100, 20);
 		contentPane.add(txtSpielername3);
--- MCG Quizchampion/src/HauptKlasse.java
+++ MCG Quizchampion/src/HauptKlasse.java
@@ -2,6 +2,12 @@
 public class HauptKlasse {
 	
 	private static int spielerZahl = 0;
+	public static Fragenkatalog fragenkatalog = new Fragenkatalog();
+	private static String spielername1, spielername2,spielername3;
+	private static int punkteS1 = 0;
+	private static int punkteS2 = 0;
+	private static int punkteS3 = 0;
+	private final static int anzahlSchwierigkeitsgrade = 3;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,6 +20,66 @@ public class HauptKlasse {
 
 	public static void setSpielerZahl(int spielerZahl) {
 		HauptKlasse.spielerZahl = spielerZahl;
+	}
+
+	public static Fragenkatalog getFragenkatalog() {
+		return fragenkatalog;
+	}
+
+	public static void setFragenkatalog(Fragenkatalog fragenkatalog) {
+		HauptKlasse.fragenkatalog = fragenkatalog;
+	}
+
+	public static String getSpielername1() {
+		return spielername1;
+	}
+
+	public static void setSpielername1(String spielername1) {
+		HauptKlasse.spielername1 = spielername1;
+	}
+
+	public static String getSpielername2() {
+		return spielername2;
+	}
+
+	public static void setSpielername2(String spielername2) {
+		HauptKlasse.spielername2 = spielername2;
+	}
+
+	public static String getSpielername3() {
+		return spielername3;
+	}
+
+	public static void setSpielername3(String spielername3) {
+		HauptKlasse.spielername3 = spielername3;
+	}
+
+	public static int getPunkteS1() {
+		return punkteS1;
+	}
+
+	public static void setPunkteS1(int punkteS1) {
+		HauptKlasse.punkteS1 = punkteS1;
+	}
+
+	public static int getPunkteS2() {
+		return punkteS2;
+	}
+
+	public static void setPunkteS2(int punkteS2) {
+		HauptKlasse.punkteS2 = punkteS2;
+	}
+
+	public static int getPunkteS3() {
+		return punkteS3;
+	}
+
+	public static void setPunkteS3(int punkteS3) {
+		HauptKlasse.punkteS3 = punkteS3;
+	}
+
+	public static int getAnzahlschwierigkeitsgrade() {
+		return anzahlSchwierigkeitsgrade;
 	}
 
 }
