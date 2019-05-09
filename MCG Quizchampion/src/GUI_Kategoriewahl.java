1eaaaa57e952e6c7124e6a432496e268e7317c23
artur.haack <artur.haack@r211-145.schule.local>
2019-05-09 10:39:53 +0200

    GUI_Kategoriewahl added Stuff

--- MCG Quizchampion/src/GUI_Kategoriewahl.java
+++ MCG Quizchampion/src/GUI_Kategoriewahl.java
@@ -4,10 +4,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUI_Kategoriewahl extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel lblKat;
+	private JButton btnKategorie1;
+	private JButton btnKategorie2;
+	private JButton btnKategorie3;
 
 	/**
 	 * Launch the application.
@@ -33,8 +41,27 @@ public class GUI_Kategoriewahl extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblKat = new JLabel("Waehle eine Kategorie");
+		lblKat.setBounds(184, 0, 48, 14);
+		contentPane.add(lblKat);
+		
+		btnKategorie1 = new JButton("");
+		btnKategorie1.setBounds(162, 28, 89, 23);
+		contentPane.add(btnKategorie1);
+		
+		btnKategorie2 = new JButton("");
+		btnKategorie2.setBounds(162, 112, 89, 23);
+		contentPane.add(btnKategorie2);
+		
+		btnKategorie3 = new JButton("");
+		btnKategorie3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnKategorie3.setBounds(162, 181, 89, 23);
+		contentPane.add(btnKategorie3);
 	}
-
 }
